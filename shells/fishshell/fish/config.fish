d8ee38a7acca5b478c83c5de8350b86ab209bc1a
Kirill Funtov <funtaps@gmail.com>
2016-08-28 13:00:46 +0300

    Configs + fisher

--- shells/fishshell/fish/config.fish
+++ shells/fishshell/fish/config.fish
@@ -5,7 +5,7 @@ set -gx PATH $HOME/bin $PATH
 set -gx PATH $HOME/.composer/vendor/psy/psysh/bin $PATH
 
 # Fisherman
-set fisher_home ~/.local/share/fisherman
+set fisher_home ~/home-gitignore/dotfiles/shells/fishshell/fisher
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
 
